428ea532221778eb99493eb2504b6e7fbc9a4837
JellyLu <634725546@qq.com>
2016-07-14 22:04:41 +0800

    Initial Commit

---
+++ Day 01 - SimpleStopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day 01 - SimpleStopWatch
+//
+//  Created by luyang on 7/14/16.
+//  Copyright © 2016 lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
